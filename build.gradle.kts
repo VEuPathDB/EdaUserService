d9f930bac996993a79d0f733d2386c94bb25f629
Dan Galdi <dmgaldi@upenn.edu>
2023-02-09 15:42:50 -0500

    Use the latest version of fgputil and container core

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,9 +78,9 @@ repositories {
 //
 
 // versions
-val coreLib       = "6.13.2"         // Container core lib version
-val edaCommon     = "10.3.1"         // EDA Common version
-val fgputil       = "2.12.0-jakarta" // FgpUtil version
+val coreLib       = "6.14.1"        // Container core lib version
+val edaCommon     = "10.3.2"        // EDA Common version
+val fgputil       = "2.12.2-jakarta" // FgpUtil version
 
 // use local EdaCommon compiled schema if project exists, else use released version;
 //    this mirrors the way we use local EdaCommon code if available
